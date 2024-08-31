66229e7ae85f4a9a9045e416ede74ff9b0ff4dc7
Prakash Kumar Singh <prakashaug98@gmail.com>
2024-08-31 22:18:22 +0530

    Add logic to retrieve data from url

--- graphql/resolvers/bookmarks.js
+++ graphql/resolvers/bookmarks.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
 const { AuthenticationError } = require("apollo-server");
 
 const Bookmark = require("../../models/Bookmark");
@@ -31,12 +33,73 @@ module.exports = {
     async createBookmark(_, { url }, context) {
       const user = checkAuth(context);
 
-      if (args.url.trim() === "") {
+      if (url.trim() === "") {
         throw new Error("Url must not be empty");
       }
 
+      // Fetch the HTML content of the URL
+      let html;
+      try {
+        const response = await axios.get(url);
+        html = response.data;
+      } catch (err) {
+        throw new Error("Failed to fetch URL content");
+      }
+
+      // Load the HTML content into cheerio
+      const $ = cheerio.load(html);
+
+      // Extract and clean up metadata
+      let title = $("title").text().trim() || "No Title";
+
+      // remove symbols and extraneous text
+      title = title
+        .replace(/\|.*/g, "")
+        .replace(/[\u21d2\u25ba\u25b6\u25b8\u25b4\u2192\u203a]+/g, "");
+
+      const description =
+        $('meta[name="description"]').attr("content") || "No Description";
+
+      // Extract and prioritize favicon URLs based on size
+      let image;
+      const faviconLinks = $(
+        'link[rel="apple-touch-icon"], link[rel="icon"], link[rel="shortcut icon"]'
+      );
+
+      let largestSize = 0;
+      faviconLinks.each((_, elem) => {
+        const sizes = $(elem).attr("sizes");
+        let size = 0;
+
+        if (sizes) {
+          // Get the largest dimension (width or height)
+          size = Math.max(...sizes.split("x").map(Number));
+        }
+
+        if (size > largestSize) {
+          largestSize = size;
+          image = $(elem).attr("href");
+        }
+
+        // Default to the first icon if sizes attribute is not present
+        if (!sizes && !image) {
+          image = $(elem).attr("href");
+        }
+      });
+
+      // Convert relative URL to absolute URL
+      if (image && !image.startsWith("http")) {
+        const urlObject = new URL(url);
+        image = urlObject.origin + image;
+      } else if (!image) {
+        image = "default-image-url"; // Fallback image URL
+      }
+
       const newBookmark = new Bookmark({
+        image,
         url,
+        title,
+        description,
         user: user.id,
         username: user.username,
         createdAt: new Date().toISOString(),
