b076c04d8d309f54fda41f8d44338ec1f0ad19c5
Prakash Kumar Singh <prakashaug98@gmail.com>
2024-09-01 18:36:51 +0530

    Add Popup for trash icon

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
-import { Button, Confirm, Icon } from "semantic-ui-react";
+import { Button, Confirm, Icon, Popup } from "semantic-ui-react";
 
 import { FETCH_BOOKMARKS_QUERY } from "../util/graphql";
 
@@ -32,14 +32,20 @@ function DeleteButton({ bookmarkId }) {
   });
   return (
     <>
-      <Button
-        as="div"
-        color="red"
-        size="mini"
-        onClick={() => setConfirmOpen(true)}
-      >
-        <Icon name="trash" style={{ margin: 0 }} />
-      </Button>
+      <Popup
+        content="Delete bookmark"
+        inverted
+        trigger={
+          <Button
+            as="div"
+            color="red"
+            size="mini"
+            onClick={() => setConfirmOpen(true)}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        }
+      />
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
